ef70e57fa5d23e8118fb615fa2113b3738fb3ede
Dhyey Shah <83173420+Dhyey181101@users.noreply.github.com>
2023-12-23 14:32:50 -0500

    Create insertionSort.cpp

---
+++ insertionSort.cpp
@@ -0,0 +1,13 @@
+void insertionSort(int arr[], int n)
+{
+    for (int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j]<arr[j-1]){
+            int temp=0;
+            temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+            j--;
+        }
+    }
+}
